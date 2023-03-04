a6216603ebe5313596b1ab532b75a878162d1ee4
ManarEltokhey <95526721+ManarEltokhey@users.noreply.github.com>
2023-03-05 00:25:44 +0200

    aboutsection

--- components/HomePage/aboutUs.js
+++ components/HomePage/aboutUs.js
@@ -1,11 +1,12 @@
 import React from "react"
-import { Button, Text, View ,Image} from "react-native";
-
+import {Text, View ,Image} from "react-native";
+import { Button } from 'react-native-paper';
 import aboutStyle from "./aboutUsStyle.js";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const Aboutus=()=>{
     return(
         <> 
-        <View style={aboutStyle.fullAboutView} >
+        {/* <View style={aboutStyle.fullAboutView} >
         <View >
          <View style={aboutStyle.aboutView}>
             <View>
@@ -24,17 +25,38 @@ const Aboutus=()=>{
               <br/>
               <Text>Follow us</Text>
        
-              {/* <a href="https://www.facebook.com/"  style={{textDecoration:"none"}}>  <i class="fa-brands fa-facebook icon w-5"></i></a>
-              <a href="https://twitter.com/?lang=en" style={{textDecoration:"none"}}> <i class="fa-brands fa-twitter icon w-5"></i></a>
-              <a href="https://www.instagram.com/" style={{textDecoration:"none"}}>  <i class="fa-brands fa-instagram icon w-5"></i></a>
-                 */}
+            
             
     
             
            
             <Image source={require('../../assets/education.jpg')} style={aboutStyle.imgStyle2}/>
             </View>
-            </View> </View><br/><br/>
+            </View> </View><br/><br/> */}
+            <View style={aboutStyle.fullAboutView}>
+            <View style={aboutStyle.aboutView} >
+            <h1 style={aboutStyle.h1Styles}>
+             About us
+            </h1>
+            <Text style={aboutStyle.paragraph}>
+            We train, qualify and enable the teacher to teach the new curriculum edu 2.0 by providing the teacher with courses, books and articles by experts in the field and also professional curriculums through our platform that will be always available for him. and give him certificates by the end of each course that will be added points to give him rewards and boosters
+            </Text>
+            <br/>
+            <Button  mode="contained" onPress={() => console.log('Pressed')} style={aboutStyle.buttonStyle}>
+            Get started
+  </Button>
+  <View style={aboutStyle.icon}>
+      <MaterialCommunityIcons name="facebook" color={"#010149"} size={30} onPress={() => Linking.openURL('https://www.facebook.com/')} />
+        <MaterialCommunityIcons name="twitter" color={"#010149"} size={30} onPress={() => Linking.openURL('https://www.twitter.com/')} />
+        <MaterialCommunityIcons name="instagram" color={"#010149"} size={30} onPress={() => Linking.openURL('https://www.instagram.com/')}/>
+        <MaterialCommunityIcons name="linkedin" color={"#010149"} size={30}  onPress={() => Linking.openURL('https://www.linkedin.com/')}/>
+        </View>
+            </View>
+            <View style={aboutStyle.imgViewStyle}>
+            <Image source={require('../../assets/education.jpg')} style={aboutStyle.imgStyle2}/>
+            </View>
+            </View>
+            <br/>
         </>
     )
 }
--- components/HomePage/aboutUsStyle.js
+++ components/HomePage/aboutUsStyle.js
@@ -3,48 +3,58 @@ import {StyleSheet} from "react-native"
 export default StyleSheet.create({
  
   imgStyle2:{
-      height: '400px',
-      width:'40%',
-      display:'flex',
+      height: '350px',
+      width:'700px',
+    resizeMode:'stretch'
+    
       
   },
   h1Styles:{
       color: '#010149' ,
-      textAlign:'center'
+      textAlign:'center',
+      fontWeight:'bold',
+      fontSize:'30px'
   },
   icon:{
-      width: '40px',
-      height: '40px',
-     lineHeight: '40px',
-      borderRadius: '50%',
-      textAlign: 'center',
-     backgroundColor:'#E1E1E1',
-      color: '#010149',
-      transition: '0.5s',
+      display:'flex',
+      flexDirection:'row',
+      justifyContent:'space-around',
+      marginBottom:5,
+      marginTop:'30px'
     },
-  // hover:{
-  //     cursor: pointer;
-  //     background-color:  #1eb2a6;
-  //     color: #FFF;
-  // },
   backColor:{
     backgroundColor: '#010149' ,
      color: '#FFF'
   },
   aboutView:{
     display:'flex',
-   
-    justifyContent: "space-evenly",
-    width:'500px'
+   flexDirection:'column',
+   marginTop:'10px'
   },
   fullAboutView:{
     display:'flex',
     flexDirection:'row',
     justifyContent: "space-evenly",
-    width:'500px'
+    marginTop:'20px'
+   
   },
   paragraph:{
-    width:'200px'
+    width:'500px',
+    marginLeft:'50px',
+    fontSize:'17px'
+  },
+  buttonStyle:{
+    width:'25%',
+    justifyContent:'center',
+    alignItems:'center',
+    marginLeft:'210px',
+    backgroundColor: '#010149'
+  },
+  imgViewStyle:{
+    display:'flex',
+    flexDirection:'column',
+    justifyContent: "space-evenly",
+   
   }
   
 })
