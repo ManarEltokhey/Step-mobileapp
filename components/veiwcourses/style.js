9ebdce49f984c905df55bd98e357d4d715cc2358
Amira-badwi <83235532+Amira-badwi@users.noreply.github.com>
2023-03-06 01:03:40 +0200

    add books

---
+++ components/veiwcourses/style.js
@@ -0,0 +1,50 @@
+export default   styles=StyleSheet.create({
+    logo:{
+       width:"250px" ,
+       height:"250px" ,
+       paddingTop:"100px",
+       margin:"auto"  ,
+       borderRadius:"5px"
+    },
+    input: {
+      height: 30,
+      width:"240px" ,
+      margin: "auto",
+      marginTop:"10px",
+      borderWidth: 1,
+      padding: 10,
+      backgroundColor:"cornsilk",
+      borderRadius:"25px"
+    },
+    container:
+    {flexDirection:"row" ,  margin:"auto"   ,justifyContent:"space-between" 
+    ,backgroundColor:"silver" , width:"250px" ,
+    marginBottom: "10px",
+    borderRadius:"25px"
+    ,      
+  }  ,
+    title:{
+        marginBottom: "10px" ,
+        width:"200px" ,
+        textAlign:"center" ,
+        fontWeight:"bold",
+        backgroundColor:"wheat" ,
+        margin:"auto"  ,
+        minHeight:"30px",
+        borderRadius:"25px"
+    }
+    , h2:{
+        color:"white" ,
+        fontWeight:"bold",
+        backgroundColor:"red",
+        borderRadius:"25px"
+    }
+   ,text2:{
+    color:"white" ,
+    fontWeight:"bold" ,
+    backgroundColor:"black",
+    borderRadius:"25px" ,
+    width:"100px" ,
+   textAlign:"center"
+   }
+})
